e991951862ee727e132b6194242ad6b0bd300ccd
Omri Shapira <omri.shapira@gmail.com>
2024-08-21 22:11:11 +0300

    Initial Commit

---
+++ Mario/MarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioApp.swift
+//  Mario
+//
+//  Created by Omri Shapira on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
